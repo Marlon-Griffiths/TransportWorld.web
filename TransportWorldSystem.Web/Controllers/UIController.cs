814f044025e6b8a473b30e0eb822a7231380c7f9
Marlon-Griffiths <Fiths2025@gmail.com>
2025-03-20 22:17:07 -0500

    Add project files.

---
+++ TransportWorldSystem.Web/Controllers/UIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransportWorldSystem.Web.Controllers
+{
+    public class UIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
