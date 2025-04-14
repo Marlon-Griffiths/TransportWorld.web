a6276f8b90df4e6a476b722e7bc047570a312655
Kennyb600 <Kenielbarclay15@gmail.com>
2025-04-13 23:56:42 -0500

    CREATED DATABASE & LINK TO ONLINE SERVER

--- TransportWorldSystem.Web/Models/RideRequestModel.cs
+++ TransportWorldSystem.Web/Models/RideRequestModel.cs
@@ -12,8 +12,6 @@
         public string DriverName { get; set; }
         public string DriverImageUrl { get; set; } // URL to the driver's picture
         public int EstimatedTime { get; set; }     // in minutes
-
-        // 👇 Add this new property to track which driver is currently selected
-        public int DriverIndex { get; set; }
+        public int DriverIndex { get; internal set; }
     }
 }
